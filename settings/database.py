d52c2ffc9543b99837c0ad3f334f0391d0c636fc
fahad39039 <42373870+fahad39039@users.noreply.github.com>
2020-02-06 17:09:31 +0530

    Update database.py

--- settings/database.py
+++ settings/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('mysql://root:root@localhost/quizapp', convert_unicode=True)
+engine = create_engine('postgres://jorzhcaxijshws:a399999444da0837dc9d6b4c362f33be25fe0bb1d03001d54843f75b93233b2e@ec2-35-172-85-250.compute-1.amazonaws.com
+/d6pspujea2rin9', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
